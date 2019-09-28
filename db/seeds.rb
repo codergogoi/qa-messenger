3c7b7626ff46ec4be55f9ef3f8e2d4d2663d1b17
codergogoi <codergogoi@gmail.com>
2019-09-28 14:17:16 +0530

    Added user login, logout, messages model features

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "tester1", password: "password")
+User.create(username: "tester2", password: "password")
+User.create(username: "tester3", password: "password")
+User.create(username: "tester4", password: "password")
+User.create(username: "tester5", password: "password")
+
+
+
+
+
